fd46d9216016b9ff2c2579d07ebcb1608ead16c5
Nikos Thomadakis <Nikos Thomadakis>
2024-03-09 18:44:23 +0200

    Added Client

---
+++ ECommerceStore.Client/src/app/layout/Header.tsx
@@ -0,0 +1,17 @@
+import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+
+interface HeaderProps {
+  darkMode: boolean;
+  handleThemeChange: () => void;
+}
+
+export default function Header({ darkMode, handleThemeChange }: HeaderProps) {
+  return (
+    <AppBar position="static" sx={{ mb: 4 }}>
+      <Toolbar>
+        <Typography variant="h6">E-Commerce Store</Typography>
+        <Switch checked={darkMode} onChange={handleThemeChange} />
+      </Toolbar>
+    </AppBar>
+  );
+}
