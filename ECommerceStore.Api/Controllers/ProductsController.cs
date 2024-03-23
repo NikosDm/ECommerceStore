68567027870930892b412d2f97734fb1a27a1b1d
Nikos Thomadakis <Nikos Thomadakis>
2024-03-23 13:52:52 +0200

    Adds filters and sorting

--- ECommerceStore.Api/Controllers/ProductsController.cs
+++ ECommerceStore.Api/Controllers/ProductsController.cs
@@ -1,5 +1,8 @@
+using System.Text.Json;
 using ECommerceStore.Api.Data;
 using ECommerceStore.Api.Entities;
+using ECommerceStore.Api.Extensions;
+using ECommerceStore.Api.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,10 +18,29 @@ namespace ECommerceStore.Api.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProductsAsync()
+        public async Task<ActionResult<PagedList<Product>>> GetProductsAsync(
+            [FromQuery] ProductParams productParams)
         {
-            var products = await _storeContext.Products.ToListAsync();
-            return Ok(products);
+            var query = _storeContext.Products
+                .Sort(productParams.OrderBy)
+                .Search(productParams.SearchTerm)
+                .Filter(productParams.Brands, productParams.Types)
+                .AsQueryable();
+
+            var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
+
+            Response.AddPaginationHeader(products.MetaData);
+
+            return products;
+        }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFiltersAsync()
+        {
+            var brands = await _storeContext.Products.Select(p => p.Brand).Distinct().ToListAsync();
+            var types = await _storeContext.Products.Select(p => p.Type).Distinct().ToListAsync();
+
+            return Ok(new { brands, types });
         }
 
         [HttpGet("{id}")]
